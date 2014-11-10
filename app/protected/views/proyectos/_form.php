f7a22b13066c0437490fe136c9e06a846cfc0a21
Alexander Vargas <alexandervargas1985@gmail.com>
2014-11-09 20:32:18 -0500

    - Mejora visual del formulario proyectos
    - Mejora del formulario de proyectos

--- app/protected/views/proyectos/_form.php
+++ app/protected/views/proyectos/_form.php
@@ -47,22 +47,20 @@
 
 		<div class="row">
 			<?php echo $form->labelEx($model,'anioInicio'); ?>
-
 				<?php 
 					echo $form->dropDownList($model,'anioInicio',
-					array('No informado'=>'No informado','Primaria Incompleta'=>'Primaria Incompleta',
-					 'Primario Secundario'=>'Primario Secundario',
-					 'Técnico - Nivel medio'=>'Técnico - Nivel medio',
-					 'Técnico nivel superior'=>'Técnico nivel superior',
-					 'Jefe de Cátedra'=>'Jefe de Cátedra',
-					 'Perfeccionamiento'=>'Perfeccionamiento',
-					 'Pregrado/Universitario'=>'Pregrado/Universitario',
-					 'Especialización'=>'Especialización',
-					 'Especialización-residencia médica'=>'Especialización-residencia médica',
-					 'Maestría/Magister'=>'Maestría/Magister',
-					 'Doctorado'=>'Doctorado',
-					 'Postdoctorado'=>'Postdoctorado',
-					 'MBA'=>'MBA',));
+					array('1980'=>'1980','1981'=>'1981',
+					 '1982'=>'1982','1983'=>'1983','1984'=>'1984',
+					 '1985'=>'1985','1986'=>'1986','1987'=>'1987',
+					 '1988'=>'1988','1989'=>'1989','1990'=>'1990',
+					 '1991'=>'1991','1992'=>'1992','1993'=>'1993',
+					 '1994'=>'1994','1995'=>'1995','1996'=>'1996',
+					 '1997'=>'1997','1998'=>'1998','1999'=>'1999',
+					 '2000'=>'2000','2001'=>'2001','2002'=>'2002',
+					 '2003'=>'2003','2004'=>'2004','2005'=>'2005',
+					 '2006'=>'2006','2007'=>'2007','2008'=>'2008',
+					 '2009'=>'2009','2010'=>'2010','2011'=>'2011',
+					 '2012'=>'2012','2013'=>'2013','2014'=>'2014',));
 				?>
 			<!-- <?php echo $form->textField($model,'anioInicio'); ?> -->
 			<?php echo $form->error($model,'anioInicio'); ?>
@@ -70,7 +68,15 @@
 
 		<div class="row">
 			<?php echo $form->labelEx($model,'mesInicio'); ?>
-			<?php echo $form->textField($model,'mesInicio',array('size'=>45,'maxlength'=>45)); ?>
+			<?php 
+					echo $form->dropDownList($model,'mesInicio',
+					array('Enero'=>'Enero','Febrero'=>'Febrero',
+					 'Marzo'=>'Marzo','Abril'=>'Abril','Mayo'=>'Mayo',
+					 'Junio'=>'Junio','Julio'=>'Julio','Agosto'=>'Agosto',
+					 'Septiembre'=>'Septiembre','Octubre'=>'Octubre',
+					 'Noviembre'=>'Noviembre','Diciembre'=>'Diciembre',));
+				?>
+			<!-- <?php echo $form->textField($model,'mesInicio'); ?> -->
 			<?php echo $form->error($model,'mesInicio'); ?>
 		</div>
 
@@ -147,17 +153,42 @@
 		<?php echo $form->textField($model,'numeroActoAdministrativo'); ?>
 		<?php echo $form->error($model,'numeroActoAdministrativo'); ?>
 	</div>
-
+	<p>
+			Fecha de fin
+	</p>
 	
 	<div class="row">
 		<?php echo $form->labelEx($model,'anioFin'); ?>
-		<?php echo $form->textField($model,'anioFin',array('size'=>45,'maxlength'=>45)); ?>
+		<?php 
+					echo $form->dropDownList($model,'anioFin',
+					array('1980'=>'1980','1981'=>'1981',
+					 '1982'=>'1982','1983'=>'1983','1984'=>'1984',
+					 '1985'=>'1985','1986'=>'1986','1987'=>'1987',
+					 '1988'=>'1988','1989'=>'1989','1990'=>'1990',
+					 '1991'=>'1991','1992'=>'1992','1993'=>'1993',
+					 '1994'=>'1994','1995'=>'1995','1996'=>'1996',
+					 '1997'=>'1997','1998'=>'1998','1999'=>'1999',
+					 '2000'=>'2000','2001'=>'2001','2002'=>'2002',
+					 '2003'=>'2003','2004'=>'2004','2005'=>'2005',
+					 '2006'=>'2006','2007'=>'2007','2008'=>'2008',
+					 '2009'=>'2009','2010'=>'2010','2011'=>'2011',
+					 '2012'=>'2012','2013'=>'2013','2014'=>'2014',));
+				?>
+			<!-- <?php echo $form->textField($model,'anioFin'); ?> -->
 		<?php echo $form->error($model,'anioFin'); ?>
 	</div>
 
 	<div class="row">
 		<?php echo $form->labelEx($model,'mesFin'); ?>
-		<?php echo $form->textField($model,'mesFin',array('size'=>45,'maxlength'=>45)); ?>
+		<?php 
+					echo $form->dropDownList($model,'mesFin',
+					array('Enero'=>'Enero','Febrero'=>'Febrero',
+					 'Marzo'=>'Marzo','Abril'=>'Abril','Mayo'=>'Mayo',
+					 'Junio'=>'Junio','Julio'=>'Julio','Agosto'=>'Agosto',
+					 'Septiembre'=>'Septiembre','Octubre'=>'Octubre',
+					 'Noviembre'=>'Noviembre','Diciembre'=>'Diciembre',));
+				?>
+			<!-- <?php echo $form->textField($model,'mesFin'); ?> -->
 		<?php echo $form->error($model,'mesFin'); ?>
 	</div>
 
